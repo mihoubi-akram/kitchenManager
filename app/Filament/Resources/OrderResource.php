0166c2127c692598bd5cb1ab70b7685a6345b651
Mihoubi akram fawzi <mihhakram@gmail.com>
2025-03-09 14:17:09 +0100

    wip

---
+++ app/Filament/Resources/OrderResource.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\OrderResource\Pages;
+use App\Filament\Resources\OrderResource\RelationManagers;
+use App\Models\Order;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class OrderResource extends Resource
+{
+    protected static ?string $model = Order::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+                ->columns([
+                    TextColumn::make('user')
+                        ->sortable()
+                        ->searchable()
+                        ->formatStateUsing(fn($record) => $record->user->name),
+                        
+    
+                    TextColumn::make('desired_delivery_date')
+                        ->sortable()
+                        ->label('Delivery Date')
+                        ->formatStateUsing(fn($record) => date('d/m/Y', strtotime($record->desired_delivery_date))),
+    
+                    TextColumn::make('status')
+                        ->sortable()
+                        ->formatStateUsing(function ($record) {
+                            return match ($record->status) {
+                                'pending' => 'Pending',
+                                'completed' => 'Completed',
+                                'cancelled' => 'Cancelled',
+                                default => 'Unknown',
+                            };
+                        }),
+    
+                    TextColumn::make('address')
+                        ->label('Delivery Address')
+                        ->sortable()
+                        ->searchable()
+                        ->formatStateUsing(fn($record) => "<em>{$record->address}</em>")
+                        ->html(),
+    
+                    TextColumn::make('fee_per_delivery')
+                        ->label('Delivery Fee')
+                        ->sortable()
+                        ->formatStateUsing(fn($record) => $record->fee_per_delivery . '$')
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListOrders::route('/'),
+            'create' => Pages\CreateOrder::route('/create'),
+            'edit' => Pages\EditOrder::route('/{record}/edit'),
+        ];
+    }
+}
